4063c39bfa2938bfffb227935f953c5b917fd2f2
Egor Ponomarev <egorponomarev.petruchcho@gmail.com>
2016-09-29 23:51:06 +0400

    setup basic libs

--- loriandroid/app/src/main/java/com/egorpetruchcho/loriandroid/ui/LoginActivity.java
+++ loriandroid/app/src/main/java/com/egorpetruchcho/loriandroid/ui/LoginActivity.java
@@ -1,8 +1,10 @@
-package com.egorpetruchcho.loriandroid;
+package com.egorpetruchcho.loriandroid.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.egorpetruchcho.loriandroid.R;
 
 public class LoginActivity extends AppCompatActivity {
 
