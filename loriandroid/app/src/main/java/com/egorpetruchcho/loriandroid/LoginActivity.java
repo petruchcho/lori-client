5ddb91d2c47f1821c8b3dc5b349470fb63de22d9
Egor Ponomarev <egorponomarev.petruchcho@gmail.com>
2016-09-29 23:05:54 +0400

    Init android app

---
+++ loriandroid/app/src/main/java/com/egorpetruchcho/loriandroid/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.egorpetruchcho.loriandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_login);
+    }
+}
+
